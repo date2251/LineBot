88e9314cafe4c5fe23f87ff1c44cedc72756f1ff
date2251 <biz76216@gmail.com>
2021-09-08 03:28:35 +0900

    オウム返しAPI

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,4 +1,39 @@
 class LinebotController < ApplicationController
-  def collback
+  require 'line/bot'
+
+  skip_before_action :verify_authenticity_token
+
+  def callback
+    body = request.body.read
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+    events = client.parse_events_from(body)
+
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          message = {
+            type: 'text',
+            text: event.message['text']
+          }
+        end
+      end
+      client.reply_message(event['replyToken'], message)
+    end
+    head :ok
+  end
+
+  private
+
+  # LINE Developers登録完了後に作成される環境変数の認証
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"] #TODO: 一旦ベタ書き
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"] #TODO: 一旦ベタ書き
+    }
   end
 end
--- test/controllers/linebot_controller_test.rb
+++ test/controllers/linebot_controller_test.rb
@@ -1,9 +1,7 @@
 require 'test_helper'
 
 class LinebotControllerTest < ActionDispatch::IntegrationTest
-  test "should get collback" do
-    get linebot_collback_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end
