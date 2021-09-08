7008fd0048c258e67960733b2001e010e287f992
date2251 <biz76216@gmail.com>
2021-09-08 09:30:09 +0900

    冗長コード削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :validate_signature, except: [:new, :create]
+
   def validate_signature
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
@@ -11,12 +12,4 @@ class ApplicationController < ActionController::Base
       error 400 do 'Bad Request' end
     end
   end  
-
-  def client
-    @client ||= Line::Bot::Client.new { |config|
-      # ローカルで動かすだけならベタ打ちでもOK。
-      config.channel_secret = "your channel secret"
-      config.channel_token = "your channel token"
-    }
-  end
 end
